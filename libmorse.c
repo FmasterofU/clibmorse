3c0ec0658491ff107bd139915ebe73d99029ff05
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-10-09 23:32:11 +0200

    sender implemented

--- libmorse.c
+++ libmorse.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -7,6 +8,17 @@
 #define ASCII_ZERO 48 // '0'
 #define ASCII_A 65  // 'A'
 #define ASCII_SPACE 32 // ' '
+#define ASCII_FSTOP_PERIOD '.'
+#define ASCII_COLON ':'
+#define ASCII_APOSTROPHE '\''
+#define ASCII_COMMA ','
+#define ASCII_EXCLAMATION_MARK '!'
+#define ASCII_QUESTION_MARK '?'
+#define ASCII_QUOTATION_MARKS '\"'
+#define ASCII_SLASH '/'
+#define ASCII_HYPHEN '-'
+#define ASCII_PLUS_SIGN '+'
+#define ASCII_EQUALS_SIGN '='
 
 static const char numbers[10][6] = {
     {"-----\0"}, //0
@@ -50,6 +62,18 @@ static const char letters[26][5] = {
     {"--..\0"}   //Z
 };
 
+static const char fullStopPeriod[6] = ".-.-.-\0";
+static const char colon[6] = "---...\0";
+static const char apostrophe[6] = ".----.\0";
+static const char comma[6] = "--..--\0";
+static const char exclamationMark[6] = "-.-.--\0";
+static const char questionMark[6] = "..--..\0";
+static const char quotationMarks[6] = ".-..-.\0";
+static const char slash[6] = "-..-.\0";
+static const char hyphen[6] = "-....-\0";
+static const char plusSign[6] = ".-.-.\0";
+static const char equalsSign[6] = "-...-\0";
+
 static unsigned char dotUnitLen = 1;
 static unsigned char dashUnitLen = 3;
 static unsigned char letterSpaceUnitLen = 3;
@@ -57,16 +81,16 @@ static unsigned char letterPartSpaceUnitLen = 1;
 static unsigned char wordSpaceUnitLen = 7;
 
 char* encode(char* text){
-    char *ret = malloc(4096);
+    char *ret = (char*) malloc(4096);
     int i, retiter = 0;
     for(i = 0; i<strlen(text); i++){
         char tempChar = (char) toupper(text[i]);
         if(tempChar == ASCII_SPACE)
             for(int k = 0; k<wordSpaceUnitLen; k++)
                 ret[retiter++]=MORSE_SIGNAL_OFF;
-        else {
-            char * tempMorseNotation = (tempChar-ASCII_ZERO<=9) ? numbers[tempChar-ASCII_ZERO] : letters[tempChar-ASCII_A];
-            for(;*tempMorseNotation != '\0';tempMorseNotation++){
+        else{
+            const char * tempMorseNotation = (tempChar-ASCII_ZERO<=9) ? numbers[tempChar-ASCII_ZERO] : letters[tempChar-ASCII_A];
+            for(; *tempMorseNotation != '\0'; tempMorseNotation++){
                 if(*tempMorseNotation=='.')
                     for(int j = 0; j<dotUnitLen; j++)
                         ret[retiter++]=MORSE_SIGNAL_ON;
@@ -81,4 +105,26 @@ char* encode(char* text){
         }
     }
     return ret;
+}
+
+void sendFrame(int consecutiveSignals, char signalType, long unitTime, void (*signal_func) (long time), void (*sleep_func)(long time)){
+    if(consecutiveSignals!=0){
+        if(signalType == MORSE_SIGNAL_ON) (*signal_func)(unitTime*consecutiveSignals);
+        else (*sleep_func)(unitTime*consecutiveSignals);
+    }
+}
+
+void send(char * dataStream, long unitTime, void (*signal_func) (long time), void (*sleep_func)(long time)){
+    int consecutiveSignals = 0;
+    char signalType = MORSE_SIGNAL_OFF;
+    for(long i = 0; i<strlen(dataStream); i++){
+        if(dataStream[i]==signalType)
+            consecutiveSignals++;
+        else if(dataStream[i]!=signalType){
+            sendFrame(consecutiveSignals, signalType, unitTime, signal_func, sleep_func);
+            consecutiveSignals=1;
+            signalType = signalType==MORSE_SIGNAL_OFF?MORSE_SIGNAL_ON:MORSE_SIGNAL_OFF;
+        }
+    }
+    sendFrame(consecutiveSignals, signalType, unitTime, signal_func, sleep_func);
 }
--- libmorse.h
+++ libmorse.h
@@ -4,5 +4,6 @@
 #define LIBMORSE_H_INCLUDED
 
 char* encode(char*);
+void send(char * dataStream, long unitTime, void (*signal_func) (long time), void (*sleep_func)(long time));
 
 #endif // LIBMORSE_H_INCLUDED
